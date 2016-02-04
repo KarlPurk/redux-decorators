b3efd96e2f6ad42e623cd7d3edb30af1a6c9fd1f
Karl Purkardt <karl.purkardt@nucleuscentral.com>
2016-02-04 20:52:25 +0000

    Added /dist folder to project

---
+++ dist/redux-decorators.js
@@ -0,0 +1,241 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("redux"));
+	else if(typeof define === 'function' && define.amd)
+		define(["redux"], factory);
+	else if(typeof exports === 'object')
+		exports["ReduxDecorators"] = factory(require("redux"));
+	else
+		root["ReduxDecorators"] = factory(root["redux"]);
+})(this, function(__WEBPACK_EXTERNAL_MODULE_4__) {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/// <reference path="../typings/tsd.d.ts" />
+	var reducer_decorator_1 = __webpack_require__(1);
+	exports.Reducer = reducer_decorator_1.Reducer;
+	var state_decorator_1 = __webpack_require__(2);
+	exports.State = state_decorator_1.State;
+	var store_decorator_1 = __webpack_require__(3);
+	exports.Store = store_decorator_1.Store;
+	var initial_state_decorator_1 = __webpack_require__(5);
+	exports.InitialState = initial_state_decorator_1.InitialState;
+	// reducer decorator module exports
+	// state decorator module exports
+	// store decorator module exports
+	// initial-state decorator module exports
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	var reducer;
+	var reducerInitialState = {};
+	var reducers = [];
+	function setInitialState(initialState) {
+	    reducerInitialState = initialState;
+	}
+	exports.setInitialState = setInitialState;
+	function getReducer() {
+	    return reducer || DefaultReducer.prototype.reducer;
+	}
+	exports.getReducer = getReducer;
+	var DefaultReducer = (function () {
+	    function DefaultReducer() {
+	    }
+	    DefaultReducer.prototype.reducer = function (state, action) {
+	        if (state === void 0) { state = reducerInitialState; }
+	        var actionReducers = reducers.filter(function (r) { return r.type === action.type; });
+	        if (actionReducers.length) {
+	            return actionReducers.reduce(function (s, r) { return Object.assign(s, r.method(s)); }, state);
+	        }
+	        return state;
+	    };
+	    return DefaultReducer;
+	})();
+	var handleActionReducer = function (target, methods) {
+	    reducers.push({
+	        type: methods[0],
+	        method: target[methods[0]]
+	    });
+	};
+	var handleRootReducer = function (target, methods) {
+	    if (target.prototype.reducer) {
+	        reducer = target.prototype.reducer;
+	    }
+	    reducers = reducers.concat(methods.map(function (m) {
+	        return {
+	            type: m,
+	            method: target.prototype[m]
+	        };
+	    }));
+	};
+	function Reducer() {
+	    var methods = [];
+	    for (var _i = 0; _i < arguments.length; _i++) {
+	        methods[_i - 0] = arguments[_i];
+	    }
+	    return function (target) {
+	        if (!target.prototype) {
+	            handleActionReducer(target, methods);
+	            return;
+	        }
+	        handleRootReducer(target, methods);
+	    };
+	}
+	exports.Reducer = Reducer;
+
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	function State() {
+	    return function (target) {
+	        console.log('@State: ', target);
+	        if (target.stateProperties === undefined) {
+	            target.stateProperties = [];
+	        }
+	        var args = Array.prototype.slice.call(arguments);
+	        args.shift();
+	        target.stateProperties = target.stateProperties.concat(args);
+	    };
+	}
+	exports.State = State;
+
+
+/***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var redux_1 = __webpack_require__(4);
+	var reducer_decorator_1 = __webpack_require__(1);
+	var appStore;
+	function getStore() {
+	    if (!appStore) {
+	        appStore = new Promise(function (resolve) {
+	            var interval = setInterval(function () {
+	                if (reducer_decorator_1.getReducer()) {
+	                    clearInterval(interval);
+	                    resolve(redux_1.createStore(reducer_decorator_1.getReducer()));
+	                }
+	            });
+	        });
+	    }
+	    return appStore;
+	}
+	exports.getStore = getStore;
+	function Store() {
+	    var properties = [];
+	    for (var _i = 0; _i < arguments.length; _i++) {
+	        properties[_i - 0] = arguments[_i];
+	    }
+	    var stateProperties = Array.prototype.slice.call(arguments);
+	    return function (target) {
+	        console.log('@Store: ', target);
+	        var existingNgOnInit = target.prototype.ngOnInit;
+	        var existingNgOnDestroy = target.prototype.ngOnDestroy;
+	        if (target.prototype.stateProperties === undefined) {
+	            target.prototype.stateProperties = [];
+	        }
+	        target.prototype.stateProperties = target.prototype.stateProperties.concat(stateProperties);
+	        target.prototype.ngOnInit = function () {
+	            var _this = this;
+	            var storeUpdateHandler = function () {
+	                var state = _this.appStore.getState();
+	                _this.stateProperties.forEach(function (prop) {
+	                    _this[prop] = state[prop];
+	                });
+	            };
+	            getStore().then(function (appStore) {
+	                _this.appStore = appStore;
+	                _this.unsubscribe = _this.appStore.subscribe(storeUpdateHandler);
+	                // Apply the default state to all listeners
+	                storeUpdateHandler();
+	            });
+	            !existingNgOnInit || existingNgOnInit();
+	        };
+	        target.prototype.dispatch = function (action) {
+	            var data = [];
+	            for (var _i = 1; _i < arguments.length; _i++) {
+	                data[_i - 1] = arguments[_i];
+	            }
+	            this.appStore.dispatch({ type: action, data: data });
+	        };
+	        target.prototype.ngOnDestroy = function () {
+	            this.unsubscribe();
+	            !existingNgOnDestroy || existingNgOnDestroy();
+	        };
+	    };
+	}
+	exports.Store = Store;
+
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	module.exports = require("redux");
+
+/***/ },
+/* 5 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var reducer_decorator_1 = __webpack_require__(1);
+	function InitialState(initialState) {
+	    reducer_decorator_1.setInitialState(initialState);
+	    console.log('@InitialState: ', initialState);
+	    return function (target) { };
+	}
+	exports.InitialState = InitialState;
+
+
+/***/ }
+/******/ ])
+});
+;
+//# sourceMappingURL=redux-decorators.js.map

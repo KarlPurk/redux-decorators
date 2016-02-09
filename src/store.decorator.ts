ca927298522c09a6dbc0458b97b5cb8ea9bf05f3
Karl Purkardt <karl.purkardt@nucleuscentral.com>
2016-02-09 14:03:37 +0000

    Refactored store decorator to improve semantics

--- src/store.decorator.ts
+++ src/store.decorator.ts
@@ -24,39 +24,67 @@ export interface IStore {
     dispatch(action);
 }
 
-// @Test export
 export function updateComponentProperties(component, state, properties = 'stateProperties') {
     component[properties].forEach(prop => {
         component[prop] = state[prop];
     });
 }
 
+function mapProperties(context) {
+    updateComponentProperties(context, context.appStore.getState())
+}
+
+function generalBinding(target, stateProperties) {
+
+    // Add stateProperties to the target
+    if (target.prototype.stateProperties === undefined) {
+        target.prototype.stateProperties = [];
+    }
+    target.prototype.stateProperties = target.prototype.stateProperties.concat(stateProperties);
+
+    // Add a dispatch method to the target
+    target.prototype.dispatch = function(action, ...data) {
+        this.appStore.dispatch({type: action, data: data});
+    };
+
+    // Add a generic store update handler
+    target.prototype.storeUpdateHandler = function() {
+        updateComponentProperties(this, this.appStore.getState());
+    };
+
+    // Add a generic storeInit method
+    target.prototype.storeInit = function() {
+        getStore().then((appStore) => {
+            this.appStore = appStore;
+            this.unsubscribe = this.appStore.subscribe(this.storeUpdateHandler);
+            // Apply the default state to all listeners
+            this.storeUpdateHandler();
+        });
+    }
+
+    // Add a generic storeDestroy method
+    target.prototype.storeDestroy = function() {
+        this.unsubscribe();
+    };
+
+    return target;
+}
+
+function angular2Binding(target) {
+    var existingNgOnInit = target.prototype.ngOnInit;
+    var existingNgOnDestroy = target.prototype.ngOnDestroy;
+    target.prototype.ngOnInit = function() {
+        this.storeInit();
+        !existingNgOnInit || existingNgOnInit();
+    }
+    target.prototype.ngOnDestroy = function() {
+        this.storeDestroy();
+        !existingNgOnDestroy || existingNgOnDestroy();
+    }
+}
+
 export function Store(...stateProperties) {
     return function(target) {
-        var existingNgOnInit = target.prototype.ngOnInit;
-        var existingNgOnDestroy = target.prototype.ngOnDestroy;
-        if (target.prototype.stateProperties === undefined) {
-            target.prototype.stateProperties = [];
-        }
-        target.prototype.stateProperties = target.prototype.stateProperties.concat(stateProperties);
-        target.prototype.ngOnInit = function() {
-            let storeUpdateHandler = () => {
-                updateComponentProperties(this, this.appStore.getState())
-            };
-            getStore().then((appStore) => {
-                this.appStore = appStore;
-                this.unsubscribe = this.appStore.subscribe(storeUpdateHandler);
-                // Apply the default state to all listeners
-                storeUpdateHandler();
-            });
-            !existingNgOnInit || existingNgOnInit();
-        }
-        target.prototype.dispatch = function(action, ...data) {
-            this.appStore.dispatch({type: action, data: data});
-        };
-        target.prototype.ngOnDestroy = function() {
-            this.unsubscribe();
-            !existingNgOnDestroy || existingNgOnDestroy();
-        }
+        angular2Binding(generalBinding(target, stateProperties));
     }
 }

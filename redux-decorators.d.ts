301f1903216e9e7d19c91d08531d0608007fd618
Karl Purkardt <karl.purkardt@nucleuscentral.com>
2016-02-17 19:51:01 +0000

    Add interfaces to ts definition file

--- redux-decorators.d.ts
+++ redux-decorators.d.ts
@@ -1,7 +1,13 @@
 declare module ReduxDecorators {
+    interface IReducer {
+        reduce(state: any, action: any);
+    }
     function InitialState(state: any): Function;
     function Reducer(...name: string[]): Function;
     function Store(...name: string[]): Function;
+    interface IStore {
+        dispatch(action: string, ...data: any[])
+    }
 }
 
 declare module "redux-decorators" {

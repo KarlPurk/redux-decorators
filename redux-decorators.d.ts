f904e0b3a8934e801e04d1cb0c90f623b4bba085
Karl Purkardt <karl.purkardt@nucleuscentral.com>
2016-06-30 20:07:43 +0100

    chore: Update the typescript definition file to cover the new @Slice decorator

--- redux-decorators.d.ts
+++ redux-decorators.d.ts
@@ -2,6 +2,7 @@ declare module ReduxDecorators {
     interface RootReducer {
         reduce(state: any, action: any);
     }
+    function Slice(slice: string, initialState: any): Function;
     function InitialState(state: any): Function;
     function Reducer(...name: string[]): Function;
     function Store(...name: string[]): Function;

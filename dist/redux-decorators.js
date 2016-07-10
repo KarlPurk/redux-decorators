f7c105fed4291ba3aacb9ec68d739bb76ff84122
Karl Purkardt <karl.purkardt@nucleuscentral.com>
2016-07-10 16:46:31 +0100

    chore: Update dist

--- dist/redux-decorators.js
+++ dist/redux-decorators.js
@@ -54,6 +54,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
+	/// <reference path="../typings/tsd.d.ts" />
 	var reducer_decorator_1 = __webpack_require__(1);
 	exports.Reducer = reducer_decorator_1.Reducer;
 	var state_decorator_1 = __webpack_require__(2);
@@ -62,6 +63,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	exports.Store = store_decorator_1.Store;
 	var initial_state_decorator_1 = __webpack_require__(7);
 	exports.InitialState = initial_state_decorator_1.InitialState;
+	var slice_decorator_1 = __webpack_require__(8);
+	exports.Slice = slice_decorator_1.Slice;
+	// reducer decorator module exports
+	// state decorator module exports
+	// store decorator module exports
+	// initial-state decorator module exports
 
 
 /***/ },
@@ -70,11 +77,9 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var rootReducer;
 	var initialState = {};
-	var actionReducers = [];
-	function setInitialState(state) {
-	    initialState = state;
-	}
-	exports.setInitialState = setInitialState;
+	//------------------------------------------------------------------------------
+	// Root reducer
+	//------------------------------------------------------------------------------
 	function setReducer(reducer) {
 	    rootReducer = reducer;
 	}
@@ -91,21 +96,46 @@ return /******/ (function(modules) { // webpackBootstrap
 	        var matchingActionTypeOnly = function (actionReducer) {
 	            return actionReducer.type === action.type;
 	        };
+	        if (action.type === '@@redux/INIT') {
+	            return actionReducers.reduce(function (nextState, reducer) {
+	                if (!reducer.owner.getInitialState) {
+	                    return state;
+	                }
+	                var initialState = reducer.owner.getInitialState(reducer.type);
+	                var slice = reducer.owner.getSlice ? reducer.owner.getSlice(reducer.methodName) : null;
+	                if (initialState !== undefined && slice) {
+	                    nextState[slice] = initialState;
+	                }
+	                return nextState;
+	            }, initialState);
+	        }
 	        var filteredActionReducers = actionReducers.filter(matchingActionTypeOnly);
 	        if (!filteredActionReducers.length) {
 	            return state;
 	        }
-	        var mutateState = function (state, actionReducer) {
-	            var mutatedState = actionReducer.fn.apply(actionReducer, [state].concat(action.data));
-	            return Object.assign(state, mutatedState);
+	        var createNextState = function (state, _a) {
+	            var owner = _a.owner, methodName = _a.methodName;
+	            var reducer = owner[methodName];
+	            var slice, nextState;
+	            slice = owner.getSlice ? owner.getSlice(methodName) : null;
+	            if (slice) {
+	                var inputState = state.hasOwnProperty(slice) ? state[slice] : undefined;
+	                nextState = state;
+	                nextState[slice] = reducer.apply(void 0, [inputState].concat(action.data));
+	            }
+	            else {
+	                nextState = reducer.apply(void 0, [state].concat(action.data));
+	            }
+	            return Object.assign(state, nextState);
 	        };
-	        return filteredActionReducers.reduce(mutateState, state);
+	        return filteredActionReducers.reduce(createNextState, state);
 	    };
 	    return DefaultReducer;
 	})();
 	exports.DefaultReducer = DefaultReducer;
-	function addActionReducer(type, fn) {
-	    actionReducers.push({ type: type, fn: fn });
+	var actionReducers = [];
+	function addActionReducer(type, owner, methodName) {
+	    actionReducers.push({ type: type, owner: owner, methodName: methodName });
 	}
 	exports.addActionReducer = addActionReducer;
 	function getActionReducers() {
@@ -116,27 +146,30 @@ return /******/ (function(modules) { // webpackBootstrap
 	    actionReducers = [];
 	}
 	exports.removeActionReducers = removeActionReducers;
-	var handleActionReducer = function (target, type) {
-	    addActionReducer(type, target[type]);
+	//------------------------------------------------------------------------------
+	// Decorator
+	//------------------------------------------------------------------------------
+	var handleActionReducer = function (owner, methodName) {
+	    addActionReducer(methodName, owner, methodName);
 	};
-	var handleRootReducer = function (target, types) {
-	    if (target.prototype.reducer) {
-	        rootReducer = target.prototype.reducer;
+	var handleRootReducer = function (owner, methodNames) {
+	    if (owner.prototype.reducer) {
+	        rootReducer = owner.prototype.reducer;
 	    }
-	    var mapTypes = function (type) { return { type: type, fn: target.prototype[type] }; };
-	    actionReducers = actionReducers.concat(types.map(mapTypes));
+	    var mapMethodNames = function (methodName) { return { type: methodName, owner: owner.prototype, methodName: methodName }; };
+	    actionReducers = actionReducers.concat(methodNames.map(mapMethodNames));
 	};
 	function Reducer() {
-	    var types = [];
+	    var methodNames = [];
 	    for (var _i = 0; _i < arguments.length; _i++) {
-	        types[_i - 0] = arguments[_i];
+	        methodNames[_i - 0] = arguments[_i];
 	    }
-	    return function (target, type) {
-	        if (!target.prototype) {
-	            handleActionReducer(target, type);
+	    return function (owner, methodName) {
+	        if (!owner.prototype) {
+	            handleActionReducer(owner, methodName);
 	            return;
 	        }
-	        handleRootReducer(target, types);
+	        handleRootReducer(owner, methodNames);
 	    };
 	}
 	exports.Reducer = Reducer;
@@ -211,10 +244,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	}
 	exports.updateStateProperties = updateStateProperties;
 	function generalBinding(target, stateProperties) {
+	    // Add stateProperties to the target
 	    if (target.prototype.stateProperties === undefined) {
 	        target.prototype.stateProperties = [];
 	    }
 	    target.prototype.stateProperties = target.prototype.stateProperties.concat(stateProperties);
+	    // Add a dispatch method to the target
 	    target.prototype.dispatch = function (action) {
 	        var data = [];
 	        for (var _i = 1; _i < arguments.length; _i++) {
@@ -222,19 +257,24 @@ return /******/ (function(modules) { // webpackBootstrap
 	        }
 	        this.appStore.dispatch({ type: action, data: data });
 	    };
+	    // Add a generic store update handler
 	    target.prototype.storeUpdateHandler = function () {
 	        updateStateProperties(this, this.appStore.getState());
 	    };
+	    // Add a generic storeInit method
 	    target.prototype.storeInit = function () {
 	        var _this = this;
 	        return this.getStore().then(function () {
 	            _this.unsubscribe = _this.appStore.subscribe(_this.storeUpdateHandler.bind(_this));
+	            // Apply the default state to all listeners
 	            _this.storeUpdateHandler();
 	        });
 	    };
+	    // Add a generic storeDestroy method
 	    target.prototype.storeDestroy = function () {
 	        this.unsubscribe();
 	    };
+	    // Add a get store method
 	    target.prototype.getStore = function () {
 	        var _this = this;
 	        if (this.appStore) {
@@ -242,6 +282,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	        }
 	        return getStore().then(function (store) { return _this.appStore = store; });
 	    };
+	    // Add a set store method for testing purposes
 	    target.prototype.setStore = function (store) {
 	        this.appStore = store;
 	    };
@@ -278,14 +319,93 @@ return /******/ (function(modules) { // webpackBootstrap
 
 /***/ },
 /* 7 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ function(module, exports) {
 
-	var reducer_decorator_1 = __webpack_require__(1);
+	var addGetInitialStateMethod = function (target) {
+	    if (target.getInitialState) {
+	        return;
+	    }
+	    target.getInitialState = function (actionType) {
+	        if (!target.initialState) {
+	            return undefined;
+	        }
+	        if (target.initialState.hasOwnProperty(actionType)) {
+	            return target.initialState[actionType];
+	        }
+	        if (target.initialState.hasOwnProperty('default')) {
+	            return target.initialState.default;
+	        }
+	        return undefined;
+	    };
+	};
+	var addInitialStateProperty = function (target) {
+	    if (target.hasOwnProperty('initialState')) {
+	        return;
+	    }
+	    target.initialState = {};
+	};
 	function InitialState(initialState) {
-	    reducer_decorator_1.setInitialState(initialState);
-	    return function (target) { };
+	    return function (target, methodName) {
+	        var isInstance = !target.prototype;
+	        if (isInstance) {
+	            addInitialStateProperty(target);
+	            addGetInitialStateMethod(target);
+	            target.initialState[methodName] = initialState;
+	            return;
+	        }
+	        addInitialStateProperty(target.prototype);
+	        addGetInitialStateMethod(target.prototype);
+	        target.prototype.initialState.default = initialState;
+	    };
 	}
 	exports.InitialState = InitialState;
+
+
+/***/ },
+/* 8 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var initial_state_decorator_1 = __webpack_require__(7);
+	var addGetSliceMethod = function (target) {
+	    if (target.getSlice) {
+	        return;
+	    }
+	    target.getSlice = function (actionType) {
+	        if (!target.stateSliceAffected) {
+	            return undefined;
+	        }
+	        if (target.stateSliceAffected.hasOwnProperty(actionType)) {
+	            return target.stateSliceAffected[actionType];
+	        }
+	        if (target.stateSliceAffected.hasOwnProperty('default')) {
+	            return target.stateSliceAffected.default;
+	        }
+	        return undefined;
+	    };
+	};
+	var addStateSliceAffectedProperty = function (target) {
+	    if (target.hasOwnProperty('stateSliceAffected')) {
+	        return;
+	    }
+	    target.stateSliceAffected = {};
+	};
+	function Slice(slice, initialState) {
+	    return function (target, method) {
+	        var isInstance = !target.prototype;
+	        if (isInstance) {
+	            addStateSliceAffectedProperty(target);
+	            addGetSliceMethod(target);
+	            target.stateSliceAffected[method] = slice;
+	            initial_state_decorator_1.InitialState(initialState)(target, method);
+	            return;
+	        }
+	        addStateSliceAffectedProperty(target.prototype);
+	        addGetSliceMethod(target.prototype);
+	        target.prototype.stateSliceAffected.default = slice;
+	        initial_state_decorator_1.InitialState(initialState)(target);
+	    };
+	}
+	exports.Slice = Slice;
 
 
 /***/ }

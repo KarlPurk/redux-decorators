6b66b4bc015742de99b36a632fbc273d36cd5f1a
Karl Purkhardt <karl.purkhardt@nucleuscentral.com>
2016-07-18 15:40:41 +0100

    chore: Update must d.ts file

--- must.d.ts
+++ must.d.ts
@@ -1,30 +1,83 @@
-declare module '~must/lib/must' {
+declare module 'must' {
 
+    interface a { (expected): Must }
+    interface an { (expected): Must }
+    interface be { (expected): Must }
     interface Must {
-        expect(): Must;
-        true(): Must;
-        property(...expected: Array<any>): Must;
-        undefined(): Must;
-        eql(expected: any): Must;
-        equal(expected: any): Must;
-        contain(expected: any): Must;
-        include(expected: any): Must;
+        a;
+        above(expected): Must;
+        after(expected): Must;
+        an;
+        array(expected): Must;
+        at: Must;
+        be;
+        before(expected): Must;
+        below(expected): Must;
+        between(begin, end): Must;
+        boolean(): Must;
+        contain(expected): Must;
+        date(expected): Must;
+        empty(): Must;
+        endWith(expected: string): Must;
+        enumerable(property: string): Must;
+        enumerableProperty(property: string): Must;
+        eql(expected): Must;
+        equal(expected): Must;
+        error(constructor?, expected?): Must;
+        eventually: Must;
+        exist(): Must;
+        false(): Must;
+        falsy(): Must;
+        frozen(): Must;
         function(): Must;
-        a: Must;
+        gt(expected: number): Must;
+        gte(expected: number): Must;
+        have: Must;
+        include(expected): Must;
+        instanceOf(expected): Must;
+        instanceof(expected): Must;
+        is(expected): Must;
+        keys(expected: Array<string>): Must;
+        least(expected): Must;
+        length(expected: number): Must;
+        lt(expected: number): Must;
+        lte(expected: number): Must;
+        match(expected): Must;
+        most(expected: number): Must;
+        must: Must;
+        nan(): Must;
+        nonenumerable(property: string): Must;
+        nonenumerableProperty(property: string): Must;
+        not: Must;
+        null(): Must;
+        number(): Must;
+        object(): Must;
+        own(property: string, value?): Must;
+        ownKeys(keys: Array<string>): Must;
+        ownProperties(properties: any): Must;
+        ownProperty(property: string, value?): Must;
+        permutationOf(expected: Array<any>): Must;
+        properties(properties: any): Must;
+        property(property: string, value?): Must;
+        regexp(): Must;
+        reject: Must;
+        resolve: Must;
+        startWith(expected: string): Must;
+        string(): Must;
+        symbol(): Must;
+        the: Must;
+        then: Must;
+        throw(constructor?, expected?): Must;
         to: Must;
-        be: Must;
+        true(): Must;
+        truthy(): Must;
+        undefined(): Must;
+        with: Must;
     }
-
     interface MustFactory {
         (target: any): Must;
     }
 
     var must: MustFactory;
-
     export = must;
-}
-
-declare module 'must' {
-    import main = require('~must/lib/must');
-    export = main;
 }
